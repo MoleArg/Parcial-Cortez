239e1b61431eb2294795a6c542531d1f1ed43754
Mole <moleroman@hotmail.com.ar>
2021-06-01 21:26:54 -0300

    Changes

--- BackEnd/src/controllers/genresController.js
+++ BackEnd/src/controllers/genresController.js
@@ -1,24 +1,15 @@
-
 const db = require('../database/models');
 const sequelize = db.sequelize;
-
 
 const genresController = {
     'list': (req, res) => {
         db.Genre.findAll()
-            .then(genres => {
-                //res.render('genresList.ejs', {genres})
-                res.json(genres)
-            })
+        .then(genres => res.json(genres))
     },
     'detail': (req, res) => {
         db.Genre.findByPk(req.params.id)
-            .then(genre => {
-                //res.render('genresDetail.ejs', {genre});
-                res.json(genre);
-            });
+            .then(genre => res.json(genre));
     }
-
 }
 
 module.exports = genresController;
